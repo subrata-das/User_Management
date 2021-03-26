99eb171e8769008ec1adc08ed3856d7e8104474e
subrata <shibanipur.s.das@gmail.com>
2021-03-26 21:19:10 +0530

    ok

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
-use App\UserInfo;
+// use App\UserInfo;
 // use Illuminate\Contracts\Cache\Store;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
